fcdc6863de4ed0762d5752d6065af0d30f40895c
aleks.valushko <aleks140393>
2018-12-12 00:04:38 +0300

    changes2

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,12 @@
-function openMenu() {
+opMenu:boolean = true;
 
+function toggle() {
+    let list = document.getElementById('list');
+    if(this.opMenu){
+        list.className = "header-menu__list-mobile";
+        this.opMenu = false;
+    } else {
+        list.className = "header-menu__list";
+        this.opMenu = true;
+    }
 }
